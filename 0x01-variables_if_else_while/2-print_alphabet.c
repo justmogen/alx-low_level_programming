4d629ea06304b68ad475dfe06ad2699f362fcaea
justmogen <justmogen@gmail.com>
2022-09-09 07:53:41 -0700

    using putchar to print a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - printing from a-z
+*Return : 0 successfull
+*/
+int main(void)
+{
+char letter = 'a';
+while (letter <= z)
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
+
