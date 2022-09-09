e3bcdffd7135d7c9a325a6021a48e084dc6aafc3
justmogen <justmogen@gmail.com>
2022-09-09 13:00:12 -0700

    combination of three different digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+*main - printing different combinations of three digits
+*Return: 0 success
+*/
+int main(void)
+{
+int x = '0';
+int y = '0';
+int z = '0';
+while (x <= '7')
+{
+while (y <= '8')
+{
+while (z <= '9')
+{
+if (x < y && y < z)
+{
+putchar('x');
+putchar('y');
+putchar('z');
+if (!(x == '7' && y == '8' && z == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+z++;
+}
+z = '0';
+y++;
+}
+y = '0';
+x++;
+}
+putchar('\n');
+return (0);
+}
