873b3381718df33ad49f35820b2253e13ddd1b2f
justmogen <justmogen@gmail.com>
2022-09-09 10:38:51 -0700

    printing with comma and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - printing all possible combinations of single-digit numbers
+*Return: 0 success
+*/
+int main{void)
+{
+int num = '0';
+while (num <= '9')
+{
+putchar(num);
+if (num != '9')
+{
+putchar(',');
+putchar(' ');
+}
+++1;
+}
+putchar('\n');
+return (0);
+}
