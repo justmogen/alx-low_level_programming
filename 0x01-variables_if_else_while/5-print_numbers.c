51dbd60c62f40558ee92b5bdeb785bfec20d7643
justmogen <justmogen@gmail.com>
2022-09-09 08:38:08 -0700

    counting 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - printing single digits numbers of base 10
+*Retur: 0 on success
+*/
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d",num);
+num++;
+}
+return (0);
+}
