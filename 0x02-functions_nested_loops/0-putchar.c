092340e0ee82ec68cab8ae20399d00e741f0fbd2
justmogen <justmogen@gmail.com>
2022-09-12 14:14:21 +0000

    printing a word using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-void print(char *str);
 /**
 *main - Prints char using putchar
-*Return: Always 0 success
+*
 *void print - function that prints the strings using putchar by incrementing the strings untill the end is reached
+*Return: returns 0 success
 */
-int main()
+int main(void)
 {
-char str[] = "_putchar";
-putchar(str);
-putchar('\n');
+char str[8] = "_putchar";
+int s;
+
+for(s = 0; s < 8; s++)
+{
+_putchar(str[s]);
+}
+_putchar('\n');
 return (0);
-}
-void print(char *str)
-{
-int i = 0;
-while(str[i] != '\0')
-{
-putchar(str[i]);
-i++;
-}
 }
