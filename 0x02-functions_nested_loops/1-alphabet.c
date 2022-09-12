6b0d98adfc3069ade6160d00b8bdde32cdd03f93
justmogen <justmogen@gmail.com>
2022-09-12 15:35:33 +0000

    printing a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: prints alphabet a-z in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+    char letr = 'a';
+    
+    while(letr <= 'z')
+    {
+	    _putchar(letr);
+	    letr++;
+    }
+    _putchar('\n');
+}
