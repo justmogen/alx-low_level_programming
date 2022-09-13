4fa2c7964fa693a2e30cdf9d6175f660010b8432
justmogen <justmogen@gmail.com>
2022-09-13 22:56:42 +0300

    computing absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+ /** 
+  * _abs - printing absolute value of the inputs
+  * @k: our integer to  input 
+  * Return: absolute value of k
+  */ 
+ int _abs(int k) 
+ { 
+         return (k * ((k > 0) - (k < 0))); 
+ }
+
