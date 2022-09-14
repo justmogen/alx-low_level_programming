b164946e2f6a22bd37e2c2f363d44c1ef54202fd
justmogen <justmogen@gmail.com>
2022-09-14 13:08:47 +0000

    multiplication of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints multiplication of 9 from 0
+ * Description:timestable for 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int y,z,answ;
+	for (z = 0; z <= 9; z++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			answ = z * y;
+			if (y == 0)
+			{
+				_putchar('0' + answ);
+			}
+			else if (answ >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + (answ / 10));
+				_putchar('0' + (answ % 10));
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + answ);
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#include <unistd.h>
-/**
- * prototypes for my original files
- */
+#ifndef main.h
+#define main.h
+
+char _putchar(char c);
 int add(int, int);
 void print_to_98(int n);
+void times_table(void);
