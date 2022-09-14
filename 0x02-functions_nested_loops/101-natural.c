2e42a83b920e4ec70d06ae3fbe5f8b73904bd32f
justmogen <justmogen@gmail.com>
2022-09-15 00:41:51 +0300

    adding multiplication of 3 and 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints multiples of 3 and 5 
+ * and their sum
+ * Return: always 0
+ */
+int main ()
+{
+	int num;
+
+	int sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3) == 0 || (num % 5) == 0)
+		{
+			sum += num;
+		}
+	}
+		printf("%d\n", sum);
+		return (0);
+	
+}
