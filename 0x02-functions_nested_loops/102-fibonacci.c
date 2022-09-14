f5bcb8e6a7cfb17f8b5a7d7e44a1b6224dbb3b23
justmogen <justmogen@gmail.com>
2022-09-15 01:31:33 +0300

    fibonacci numbers upto 50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints first 50  fibonacci numbers
+ * starting with 1 and 2
+ * Return: 0 always
+ */
+int main(void)
+{
+	int counter;
+	unsigned int fn1 = 0, fn2 = 1, sum;
+	
+	for (counter = 0; counter < 50; counter++)
+	{
+		sum = fn1 + fb2;
+		printf("%lu", sum);
+
+		fn1 = fn2;
+		fn2 = sum;
+
+		if (counter != 49)
+		{
+			printf(", ");
+		else
+			printf(" ");
+	}
+	return (0);
+}
