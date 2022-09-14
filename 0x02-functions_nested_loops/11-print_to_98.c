563cd55b481983a83966429ff4ee48237027b27a
justmogen <justmogen@gmail.com>
2022-09-14 12:00:16 +0000

    prototypes

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,24 +12,24 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 		printf("%d", n);
-		}
 		if (n != 98)
 		{
 			printf(", ");
 		}
-		n++;
+		n++
+		}
 	}
 	else if (n > 98)
 	{
 		while (n >= 98)
 		{
 		printf("%d", n);
-		}
 		if (n != 98)
 		{
 			printf(", ");
 		}
 		n--;
+		}
 	}
 	else
 	{
