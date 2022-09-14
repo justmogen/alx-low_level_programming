a3b6b21754df9dc99d7a8ff7ff83109213074089
justmogen <justmogen@gmail.com>
2022-09-14 14:17:11 +0300

    prototypes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,11 @@
  #include "main.h"
 /**
   *add - adds two input integers 
-  *
   *Description:x+y
   *
   *@num1:input
-  *
   *@num2:input
-  *
   *Return:sum of the inputs
-  *
   */
 int add(int num1, int num2)
 {
