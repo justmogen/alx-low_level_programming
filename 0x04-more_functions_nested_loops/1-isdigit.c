afe89e98286c030d3e8244f752bd5163eea4d104
justmogen <justmogen@gmail.com>
2022-09-15 12:50:30 +0000

    fifinding 0-9 digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - function to check for a digit from 0-9
+ * @c:input
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 38 && c <= 47)
+		return (1);
+	else
+		return (0);
+}
