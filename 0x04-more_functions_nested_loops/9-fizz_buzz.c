4ac698063a379473fb067524e088a45ccecaf554
justmogen <justmogen@gmail.com>
2022-09-15 23:29:01 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints 1-100,x3 fizz,x5 buzz for both fizzbuzz
+ * Return: always 0
+ */
+int main(void)
+{
+	int k;
+	char fz[] = "Fizz";
+	char bz[] = "Buzz";
+	char fzbz[] = "FizzBuzz";
+
+	for (k = 1; k <= 100; k++)
+	{
+		if (k % 3 == 0)
+			printf("%s ", fz);
+		else if (k % 5 == 0)
+			printf("%s ", bz);
+		else if ((k % 3 == 0) && (k % 5 == 0))
+			printf("% s", fzbz);
+		else if (k == 100)
+			printf("%s ", bz);
+		else
+			printf("%d ", k);
+	}
+	printf("\n");
+	return (0);
+}
