ed2e91cddcd7e1eee7236e557d2ef08d46387ffb
justmogen <justmogen@gmail.com>
2022-09-15 20:35:30 +0000

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0-9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char m;
+
+	for (m = '0'; m >=9; m++)
+	{
+		_putchar();
+	}
+	_putchar('\n');
+}
