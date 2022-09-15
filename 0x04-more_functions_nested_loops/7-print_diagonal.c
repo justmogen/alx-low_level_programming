1bcf97ba88ae21eaac97fd6d101bed2e9c322ad9
justmogen <justmogen@gmail.com>
2022-09-15 22:14:23 +0000

    print diogonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straight line on terminal
+ * @n: input
+ * Return: drraws a straight line
+ */
+void print_diagonal(int n)
+{
+	int p;
+
+	if (n > 0)
+	{
+		for (p = 1; p <= n; p++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
