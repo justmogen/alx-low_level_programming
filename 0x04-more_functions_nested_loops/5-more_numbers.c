a8e12f55f7f9246b77733e560c1dd580a104d349
justmogen <justmogen@gmail.com>
2022-09-15 21:44:37 +0000

    print 0-14 in 10 lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#
 #include "main.h"
 /**
  * more_numbers - function that prints 10 numbers from 0-14
@@ -6,11 +7,12 @@
 void more_numbers(void)
 {
 	int l, ftin;
-	for (ftin = 0; ftin <=14; ftin+=)
+
+	for (ftin = 0; ftin <= 14; ftin++)
 	{
 		for (l = 0; l < 10; l++)
 		{
-			if (ftin >= 10)
+			if (ftin > 9)
 			{
 				_putchar((ftin / 10) + '0');
 			}
