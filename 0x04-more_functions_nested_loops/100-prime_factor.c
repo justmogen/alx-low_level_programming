26c64b9bafecc51d50017fee9048367ddea72bc4
justmogen <justmogen@gmail.com>
2022-09-16 00:40:39 +0000

    prime

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints prime values
+ * Return: always 0
+ */
+int main(void)
+{
+	long sion, prn = 612852475143;
+
+	while (sion < (prn / 2))
+	{
+		if ((prn % 2) == 0)
+		{
+			prn /= 2;
+			continue;
+		}
+		for (sion = 3; sion < (prn / 2); sion += 2)
+		{
+			if ((prn % sion) == 0)
+				prn /= sion;
+		}
+	}
+	printf("%ld\n", prn);
+	return (0);
+}
