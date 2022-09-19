ce06059bdc3009537f42e1eccedafc4955d8dcd4
justmogen <justmogen@gmail.com>
2022-09-19 13:13:42 +0000

    swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps values of two integers
+ * @a: integer one
+ * @b: integer b
+ * return: swaped integers
+ */
+void swap_int(int *a, int *b)
+{
+	int s = *a;
+	*a = *b;
+	s = *b;
+}
