c145e39b6baaf64d7eb74a8e8a93af99fac6c170
justmogen <justmogen@gmail.com>
2022-09-19 19:48:46 +0000

    chnage string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to be converted
+ * Return: converted string
+ */
+int _atoi(char *s)
+{
+	int sgn = 1;
+	unsigned int mbr = 0;
+
+	do {
+		if (*s == '-')
+		{
+			sgn = sgn * -1;
+			else if (*s >= '0' && *s <= '9')
+			{
+				mbr = (mbr * 10) + (*s - '0');
+			}
+			else if (mbr > 0)
+				break;
+		}
+	} while (*s++);
+
+	return (mbr * sign);
+}
