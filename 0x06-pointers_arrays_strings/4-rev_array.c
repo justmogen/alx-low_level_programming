4934d4db34897b52a4e16cf8e9f43ea886e24be0
justmogen <justmogen@gmail.com>
2022-09-21 20:50:58 +0000

    reversing array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverses the content of array of integers
+ * @a:array of integers to be reversed
+ * @n:number of elements in array
+ * Return: i
+ */
+void reverse_array(int *a, int n)
+{
+	int i, pm;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		pm = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 -i] = pm;
+	}
+}
