61fffa6db4e4d7c206e83f039a04e5835e254a05
justmogen <justmogen@gmail.com>
2022-09-21 09:21:58 +0000

    concat the string pointed by @src to @dest

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - append the @src string to the end of @dest string
+ * @dest:string where the src string is appended to
+ * @src:string to be appended to dest
+ * Return: a pointer to the resulting string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int ndx = 0;
+
+	int leng.d = 0;
+
+	if (dest[ndx++])
+	{
+		leng.d++;
+	}
+	for (ndx = 0; src[ndx]; ndx++)
+	{
+		dest[leng.d] = src[ndx];
+	}
+	return (dest);
+}
