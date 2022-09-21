fedd5197b144dfad4ee8e53d46dbd8bff996adcc
justmogen <justmogen@gmail.com>
2022-09-21 15:45:58 +0000

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * @src:string to be appended to dest
  * Return: a pointer to the resulting string @dest
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest,const char *src)
 {
 	int ndx = 0;
 
