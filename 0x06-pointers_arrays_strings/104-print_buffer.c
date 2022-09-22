6004bf66bf5a0efc7674180ed8e2bf315857b073
justmogen <justmogen@gmail.com>
2022-09-22 21:50:13 +0000

    print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,13 +17,11 @@ void print_buffer(char *b, int size)
 		for (ndx = 0; ndx < 10; ndx++)
 		{
 			if ((ndx + Bt) >= size)
-			{
 				printf(" ");
-			}
+
 			else
-			{
 				printf("%02x", *(b + ndx + Bt));
-			}
+
 			if ((ndx % 2) != 0 && ndx != 0)
 			{
 				printf(" ");
@@ -32,21 +30,21 @@ void print_buffer(char *b, int size)
 		for (ndx = 0; ndx < 10; ndx++)
 		{
 			if ((ndx + Bt) >= size)
-			{
 				break;
-			}
+
 			else if (*(b + ndx + Bt) >= 31
 				 && *(b + ndx + Bt) <= 126)
-			{
 				printf("%c", *(b + ndx + Bt));
-			}
 			else
 				printf(".");
 		}
+
 		if (Bt >= size)
 			continue;
+
 		printf("\n");
 	}
+
 	if (size <= 0)
 		printf("\n");
 }
