306f4c49bfbe136818f8cf99832b31463bec5a13
justmogen <justmogen@gmail.com>
2022-09-22 18:01:16 +0000

    using 1337 algorithm

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 char *leet(char *s)
 {
 	int i, i2;
-	char elet[8] = {'A', '?', 'E', 'O', '?', 'T', '?', 'L'};
+	char elet[] = "aAeEoOtTlL";
+	char leet[] = "4433007711";
 
 	for (i = 0; s[i]; i++)
 	{
-		for (i2 = 0; i2 <= 7; i2++)
+		for (i2 = 0; i2 <= 9; i2++)
 		{
-			if (s[i] == elet[i2] ||
-			    s[i] - 32 == elet[i2])
-				s[i] = i2 + '0';
+			if (elet[i2] == s[i])
+				s[i] = leet[i2];
 		}
 	}
-	return (s);
+	return (0);
 }
