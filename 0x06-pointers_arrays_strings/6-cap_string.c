50288f02ada638f5166504af602385bc44fa14c6
justmogen <justmogen@gmail.com>
2022-09-22 16:50:25 +0000

    changing all first letters of all words to capital

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s:string to be capitalized
+ * Return: changed string pointer
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+			i++;
+		}
+		if (s[i - 1] == ' ' ||
+		    s[i - 1] == '\t' ||
+		    s[i - 1] == '\n' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		    s[i - 1] == '.' ||
+		    s[i - 1] == '!' ||
+		    s[i - 1] == '?' ||
+		    s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		    i == 0)
+			s[i] -= 32;
+	}
+	return (s);
+}
