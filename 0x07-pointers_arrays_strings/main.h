b1114ebf73f68d8706c2a538aaae5dbbc9e9aad9
justmogen <justmogen@gmail.com>
2022-09-26 16:40:33 +0000

    print chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,4 @@
-#ifdef MAIN_H
-#define MAIN_H
 
-#include <stddef.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -12,4 +9,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 
-#endif
+
