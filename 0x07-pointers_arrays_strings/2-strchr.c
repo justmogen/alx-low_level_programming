19561ab5fdebec0e0699af0d2af0e3325d4bb156
justmogen <justmogen@gmail.com>
2022-09-26 10:36:19 +0000

    using _strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - locate a character in a string
+ * @s:string searched
+ * @c:characer searched
+ * Return: if character found - pointer s
+ * if not - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int ndx = 0;
+
+	while (s[ndx] >= '\0')
+	{
+		if (s[ndx] == c)
+			return (s + ndx);
+	}
+	ndx++;
+	return ('\0');
+}
