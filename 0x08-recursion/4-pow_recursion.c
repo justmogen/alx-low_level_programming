e90d52add4aca99634170f76571b68e3666b8c49
justmogen <justmogen@gmail.com>
2022-09-28 22:36:17 +0300

    power of x using y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - print x raised to power y
+ * @x:input to be raised
+ * @y:power
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if(y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		ans = x * _pow_recursion(x, y - 1);
+		return (ans);
+	}
+}
