03bd9dc6e785f6c7fe5b12eacf9bbda44be239fc
justmogen <justmogen@gmail.com>
2022-09-29 00:14:30 +0300

    check for prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,6 +4,7 @@
  * @n:input num to check it
  * Return:1
  */
+int prime_num(int n, int check);
 int is_prime_number(int n)
 {
 	return (prime_num(2, n));
@@ -14,13 +15,13 @@ int is_prime_number(int n)
  * @check:used to check @n
  * Return: 1 if prime else 0
  */
-int prime_num (int n, int check)
+int prime_num(int n, int check)
 {
 	if (n % check == 0 && n <= 1)
 		return (0);
 	if (n == check - 1)
 		return (1);
-	if (base > n)
+	if (check > n)
 		return (prime_num( n + 1, check));
 	return (1);
 }
