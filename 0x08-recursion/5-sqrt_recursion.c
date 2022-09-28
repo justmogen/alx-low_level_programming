a4430d04adae2df2d5e4af39119f1308a1600541
justmogen <justmogen@gmail.com>
2022-09-28 23:27:16 +0300

    sqrroot of natural numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * sqrroot - find square root
+ * @n:input
+ * @s:tested natural number
+ * Return:square root if natural number found
+ */
+int sqrroot(int n, int s)
+{
+	if (n < 0)
+		return (-1);
+	if (n == (s * s))
+		return (s);
+	if (n < (s * s))
+		return (-1);
+	return (sqrroot(n, s + 1));
+}
+/**
+ * _sqrt_recursion - find sqrroot of natural numbers
+ * @n:input
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrroot(n, 1));
+}
