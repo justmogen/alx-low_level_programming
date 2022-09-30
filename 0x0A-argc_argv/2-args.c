9a007817044afb40ed502fbb7ded792db425c33b
justmogen <justmogen@gmail.com>
2022-09-30 17:26:00 +0000

    print arguments supplied one by one

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print all inserted arguments one by one
+ * @argc:number of supplied arguments
+ * @argv:array of pointers of arguments
+ * Return: 0 succes
+ */
+int main(int argc, char *agrv[])
+{
+	while (argc--)
+		printf("%s\n", argv[]);
+		argv++;
+	return (0);
+}
