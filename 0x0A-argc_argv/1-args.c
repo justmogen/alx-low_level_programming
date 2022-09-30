f57f798808574ac81b57ceec9bd0a874c1cf4b22
justmogen <justmogen@gmail.com>
2022-09-30 17:11:57 +0000

    printing the argument position

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the number of the strings passed as arguments
+ * @argc:number of arguments used
+ * @argv:array of pointers used in arguments
+ * Return: 0 success
+ */
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
