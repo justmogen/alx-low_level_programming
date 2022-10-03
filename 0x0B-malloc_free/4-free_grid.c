a5ab4940ccbe985e17fcfd63e51fa0ef8f8d83fc
justmogen <justmogen@gmail.com>
2022-10-03 21:30:11 +0000

    free()

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * free_grid - function that frees a 2DM array
+ * @grid:the 2DM array to be freed
+ * @height:height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int ndx;
+	for (ndx = 0; ndx < height; ndx++)
+		free(grid[ndx]);
+
+	free(grid);
+}
