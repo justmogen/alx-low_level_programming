ef6c670764e0cea38e6d15729626e548d060b37b
justmogen <justmogen@gmail.com>
2022-10-04 12:15:17 +0000

    concatenate different number of arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,7 +8,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int inp1, inp2, ndx = 0;
+	int inp1, inp2, ndx = 0, count = ac;
 	char *conc_strg;
 
 	if (ac == 0 || av == NULL)
@@ -19,10 +19,10 @@ char *argstostr(int ac, char **av)
 	for (inp1 = 0; inp1 < ac; inp1++)
 	{
 		for (inp2 = 0; av[inp1][inp2]; inp2++)
-			ac++;
+			count++;
 	}
 
-	conc_strg = malloc(sizeof(char) * ac + 1);
+	conc_strg = malloc(sizeof(char) * count + 1);
 
 	if (conc_strg == NULL)
 		return (NULL);
@@ -39,7 +39,7 @@ char *argstostr(int ac, char **av)
 		ndx++;
 	}
 
-	conc_strg[ac] = '\0';
+	conc_strg[count] = '\0';
 
 	return (conc_strg);
 }
