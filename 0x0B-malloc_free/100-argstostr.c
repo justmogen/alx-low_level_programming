e7fa9428d22f4ca36f455c0f1d6b1a437b520258
justmogen <justmogen@gmail.com>
2022-10-04 11:39:21 +0000

    concatenate different number of arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - It concatenates all the arguments of the program
+ * @ac:number of arguments supplied to program
+ * @av:array pointers to the arguments
+ * Return: pointer to new string created by concat the arguments
+ */
+char *argstostr(int ac, char **av)
+{
+	int inp1, inp2, ndx = 0;
+	char *conc_strg;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (inp1 = 0; inp1 < ac; inp++)
+	{
+		for (inp2 = 0; av[inp1][inp2]; inp++)
+			ac++;
+	}
+
+	conc_strg = malloc(sizeof(char) * ac + 1);
+
+	if (conc_strg == NULL)
+		return (NULL);
+
+	else
+		conc_strg[ndx++] = av[inp1][inp2];
+	conc_stg[ndx++] = '\n';
+
+	conc_strg = '\0';
+
+	return (conc_strg);
+}
