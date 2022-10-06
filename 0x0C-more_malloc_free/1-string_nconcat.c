899eafcbcc8697e050c23a9ff3ee19729ff0ee30
justmogen <justmogen@gmail.com>
2022-10-06 19:50:24 +0000

    concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l = n, ndx, l2 = 0;
+	unsigned int l = n, ndx;
 	char *conc;
 
 	if (s1 == NULL)
@@ -22,16 +22,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		l++;
 	}
-	conc = malloc(sizeof(char) * l + 1);
+	conc = malloc(sizeof(char) * (l + 1));
 
 	if (conc == NULL)
 		return (NULL);
+	l = 0;
 
 	for (ndx = 0; s1[ndx]; ndx++)
-		conc[l2++] = s1[ndx];
+		conc[l++] = s1[ndx];
 
 	for (ndx = 0; s2[ndx] && ndx < n ; ndx++)
-		conc[l2++] = s2[ndx];
+		conc[l++] = s2[ndx];
 	/**
 	 * ndx < n: copies the specified number of bytes to the s1 string
 	 * (a part of the string) and locates the required space in memory
