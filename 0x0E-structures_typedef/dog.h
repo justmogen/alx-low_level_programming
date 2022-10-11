3d6fb6b20df97e2454b361ef9286abda0fbb347b
justmogen <justmogen@gmail.com>
2022-10-11 13:46:25 +0000

    structure of dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,9 +12,8 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog t_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 #endif
