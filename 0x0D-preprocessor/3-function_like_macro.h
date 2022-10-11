fdd2d4657f6162bc595f74af9fa969c7fad0ba4e
justmogen <justmogen@gmail.com>
2022-10-11 22:08:58 +0000

    macro-like ABS for finding absolute values

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * Desc:ABS(x) - headerfile that computes the
+ *	absolute value of a given number
+ *
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
