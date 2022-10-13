663505bd661b234f92bf2818aa39e4a6ba829dba
justmogen <justmogen@gmail.com>
2022-10-13 17:22:22 +0000

    executing a function given as a parameter in an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,10 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (action == NULL || array == NULL)
 		return;
-	while (size > 0)
+	while (size-- > 0)
 	{
 		action(*array);
 		array++;
-		size--;
 	}
 }
