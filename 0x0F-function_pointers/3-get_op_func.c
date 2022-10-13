ae3413484f476ed84dcbd202ebf6540a937dcc1e
justmogen <justmogen@gmail.com>
2022-10-13 22:33:47 +0000

    perfom some operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,7 +11,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	p_t ops[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -20,6 +20,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 	int i = 0;
+
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
