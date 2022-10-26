e2001ed04c073d4c090e1a922655d64bcc6926a9
justmogen <justmogen@gmail.com>
2022-10-26 21:47:10 +0000

    elements of list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,13 @@
 size_t print_listint(const listint_t *h)
 {
 	listint_t *head = h;
+	size_t count = 0;
 
 	while (head != NULL)
 	{
+		count++;
 		printf("%d\n", head->n);
 		head = head->next;
 	}
+	return (count);
 }
-
