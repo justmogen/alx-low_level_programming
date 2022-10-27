37306309ee8fd9e26294d0c222b7ade3a21cbd91
justmogen <justmogen@gmail.com>
2022-10-27 12:43:32 +0000

    add new node at end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,9 +10,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *nNew, *lNode;
 
-	nNew= malloc(sizeof(listint_t));
+	nNew = malloc(sizeof(listint_t));
 	nNew->n = n;
 	nNew->next = NULL;
+	if (nNew == NULL)
+		return (NULL);
 
 	/* if head is null,make it an empty list */
 	if (*head == NULL)
