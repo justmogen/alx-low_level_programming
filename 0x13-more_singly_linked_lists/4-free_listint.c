9932a45ca413847a191592c02fe17be1e02d5487
justmogen <justmogen@gmail.com>
2022-10-27 13:00:01 +0000

    free the list memory

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * @head:head of list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
