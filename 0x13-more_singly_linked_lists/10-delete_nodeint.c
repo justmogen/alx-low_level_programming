dbcf116f02bcf7d74217f84bd36a99606d648453
justmogen <justmogen@gmail.com>
2022-10-27 22:25:42 +0000

    delete a value using index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,16 +3,17 @@
  * delete_nodeint_at_index - deletes the node at index index of a listint_t linked list.
  * @head: pointer to head of list
  * @index: index of node to be deleted
- * Returns: 1 if it succeeded, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	//store head node in tempo
 	listint_t *tempo = *head, *prev;
-	unsigned int count = 0;
+	unsigned int count;
 
 	if (tempo == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next; //changed head
@@ -20,14 +21,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	
-	while (count < (index - 1))
+	for (count = 0; count < (index - 1); count++ )
 	{
-		count++;
 		if (tempo->next == NULL)
 			return (-1);
 
 		tempo = tempo->next;
 	}
+
 	prev = tempo->next;
 	tempo->next = prev->next; //unlink the node from linked list
 	free(prev);
