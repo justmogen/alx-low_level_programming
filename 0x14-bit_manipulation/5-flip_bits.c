5947041163f5ca47a3b1805fc4e5e2aec46672f4
justmogen <justmogen@gmail.com>
2022-10-31 20:27:51 +0000

    number of bits to flip to make n -- m

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns the number of bits you would need
+ *		to flip to get from one number to another
+ * @n:number to be flipped
+ * @m:nuber to flip n to
+ * Calculate (n xOR m), since 0 xOR 1 and 1 xOR 0 is equal to 1.
+ *		So calculating the number of set bits in n xOR m will give us the
+ *		count of the number of unmatching bits in n and m,
+ *		which needs to be flipped
+ *
+ * Return:number  of bits to flip to get from n to m
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int
+		count = 0, xOR = n ^ m;
+
+	while (xOR > 0)
+	{
+		count += (xOR & 1);
+		xOR >>= 1;
+	}
+	return (count);
+}
+
