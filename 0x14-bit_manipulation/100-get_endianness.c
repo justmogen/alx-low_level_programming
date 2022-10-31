a93160b621ade716c6470b3e03bbaf38138a4e87
justmogen <justmogen@gmail.com>
2022-10-31 20:44:02 +0000

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_endianness - function that checks the endianness.
+ * Endianness means that the bytes in computer memory are
+ *		read in a certain order.
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int count = 1;
+	char *end = 1 = (char *)&num;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
