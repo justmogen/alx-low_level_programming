2856b0b4cebf959f817815fbe1d4bd98cdaa6910
justmogen <justmogen@gmail.com>
2022-11-07 14:37:49 +0000

    correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,8 +19,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	file = open(filename, 0_RDONLY);
 
-	if (file == NULL)
-		return (0);
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
