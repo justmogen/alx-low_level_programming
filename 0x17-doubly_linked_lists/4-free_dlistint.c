ecc57ca3b2ab01571926e62c187007bcdb293613
justmogen <justmogen@gmail.com>
2022-12-08 20:48:57 +0000

    free a node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a linked list
+ * @head: head of list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tempo;
+
+	while(head)
+	{
+		tempo = head->next;
+		free(head);
+		head = tempo;
+	}
+}
