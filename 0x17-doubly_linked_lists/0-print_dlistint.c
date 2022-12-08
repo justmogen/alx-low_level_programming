f8ef9a9701bc3293ef61e737a57d671265bf3019
justmogen <justmogen@gmail.com>
2022-12-08 21:24:46 +0000

    correction

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
 
-	while(h)
+	while (h)
 	{
 		num++;
-		print("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (num);
