0b4804c25aa00bc3617fa9f31004013e2c719c69
justmogen <justmogen@gmail.com>
2023-02-23 18:15:03 +0000

    perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ This is an island measuring function """
 
+
 def island_perimeter(grid):
     """ Returns the perimeter of the island.
     The grid repr.. land by 1 and water by 0
@@ -14,10 +15,10 @@ def island_perimeter(grid):
     for x in range(rows):
         for y in range(cols):
             if grid[x][y] == 1:
-                perimeter += 4 # Add 4 to perimeter for each land cell
+                perimeter += 4  # Add 4 to perimeter for each land cell
                 if x > 0 and grid[x-1][y] == 1:
-                    perimeter -= 2 # remove 2 if there is a land cell above
+                    perimeter -= 2  # remove 2 if there is a land cell above
                 if y > 0 and grid[x][y-1] == 1:
-                    perimeter -= 2 # remove 2 if there is a land cell to the left
+                    perimeter -= 2  # remove 2 if there is a land cell to left
 
     return perimeter
