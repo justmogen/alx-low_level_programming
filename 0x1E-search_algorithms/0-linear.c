c846af046bd2cefc51a056bc7a306b73b05038f6
justmogen <justmogen@gmail.com>
2023-07-14 06:26:20 +0300

    using linear algorithm

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search  - uses linear search algo to search for a value in an array
+ * @array: inserted array
+ * @size: size of array
+ * @value: one to be checked on the array
+ *
+ * Return: 0
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t c;
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (c=0; c < size; c++)
+	{
+		printf("Value checked array[%li] = [%i]\n", c, array[c]);
+		if (array[c] == value)
+		{
+			return (c);
+		}
+	}
+	return (-1);
+}	
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+Different Search algorithms problems"
