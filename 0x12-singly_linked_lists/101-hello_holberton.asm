3ff48b3850759ff3a4fcbd22bc28cc2b5a0e5e52
justmogen <justmogen@gmail.com>
2024-02-24 21:51:13 +0300

    seems the error persists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,15 @@
 section .data
-	hello:     db 'Hello, Holberton',10    ; 'Hello, World!' plus a linefeed character
-	helloLen:  equ $-hello             ; Length of the 'Hello world!' string
+    hello db 'Hello, Holberton', 0
 
 section .text
-	global _start
+    global main
+    extern printf
 
-_start:
-	mov eax,4            ; The system call for write (sys_write)
-	mov ebx,1            ; File descriptor 1 - standard output
-	mov ecx,hello        ; Put the offset of hello in ecx
-	mov edx,helloLen     ; helloLen is a constant, so we don't need to say
-	                     ;  mov edx,[helloLen] to get it's actual value
-	int 80h              ; Call the kernel
-	mov eax,1            ; The system call for exit (sys_exit)
-	mov ebx,0            ; Exit with return "code" of 0 (no error)
-	int 80h;
+main:
+    ; Call printf with the address of the hello string
+    mov rdi, hello     ; Format string
+    call printf
+
+    ; Exit the program
+    mov eax, 0          ; syscall: exit
+    ret
