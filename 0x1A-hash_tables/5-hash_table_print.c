d7f7acad0b42d3a39bb63b8adc928c3cd3350bdd
justmogen <justmogen@gmail.com>
2024-03-11 17:20:23 +0300

    printing the hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	char *separator = "";
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			printf("%s'%s': '%s'", separator, node->key, node->value);
+			separator = ", ";
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
