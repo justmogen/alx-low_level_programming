cae4eef286fbc043638aed457f1905f1d18d4b76
justmogen <justmogen@gmail.com>
2024-03-11 15:53:36 +0300

    the hash_djb2 function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -19,21 +19,3 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	return (hash);
 }
-
-/**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-	char *s;
-
-	s = "cisfun";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "Don't forget to tweet today";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	s = "98";
-	printf("%lu\n", hash_djb2((unsigned char *)s));
-	return (EXIT_SUCCESS);
-}
